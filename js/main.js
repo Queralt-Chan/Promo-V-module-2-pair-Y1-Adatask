d783b09288aec5035c57088d65f806938c4eeb78
Silvia Escanciano <silvia.escanciano@gmail.com>
2024-01-12 11:20:05 +0100

    class inner HTML tasks

---
+++ js/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const tasksList  = document.querySelector('.task-list');
+
+const tasks = [
+    { name: 'Recoger setas en el campo', completed: true },
+    { name: 'Comprar pilas', completed: true },
+    { name: 'Poner una lavadora de blancos', completed: true },
+    {
+      name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
+      completed: false,
+    },
+];
+
+for (const itemTask of tasks) {
+    tasksList.innerHTML += `<li>${itemTask.name}</li>`;
+}
+
+//Cómo poner el checkbox dentro del HTML
+// const checkbox = document.createElement("input");
+// checkbox.type = "checkbox";

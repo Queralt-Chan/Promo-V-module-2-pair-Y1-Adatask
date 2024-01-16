88588cf8a3ade28f4189bd3e1e57025c2ca9811d
Silvia Escanciano <silvia.escanciano@gmail.com>
2024-01-16 10:38:32 +0100

    forget this commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const tasksList  = document.querySelector('.task-list');
-
 const tasks = [
     { name: 'Recoger setas en el campo', completed: true },
     { name: 'Comprar pilas', completed: true },
@@ -13,28 +11,58 @@ const tasks = [
 ];
 
 
-for (const task of tasks) {
-  let checkbox = '<input= type="checkbox" class= "list">';
-  if (task.completed) {
-      checkbox = '<input= type="checkbox" checked>';
-  }
-  tasksList.innerHTML += `<li>${checkbox}${task.name}</li>`;
-  const inputs = document.querySelectorAll ('.list');
-  for (const input of inputs) {
-    input.addEventListener ('click', handleClick);
-    console.log ('click');
+const tasksList  = document.querySelector('.task-list');
 
-  }
-  function handleClick (){
-    if (tasks.completed === true) {
-      tasks.completed === false;
-      checkbox.classList.add ('tachado') 
-    }
-    else {
-      tasks.completed === true;
-      checkbox.classList.remove ('tachado') 
-      
-    }
-  }
+const renderTasks = () => {
+  tasksList.innerHTML = '';
+
 }
+
+for (let i = 0; i < tasks.length; i++){
+  if (tasks[i].completed) {
+    tasksList.innerHTML += `<li class="tachado"> <input type="checkbox" id="${i}" checked> ${tasks[i].name}</li>`;
+  } else {
+    tasksList.innerHTML += `<li> <input type="checkbox" class="js-check" id="${i}" ${tasks[i].name}</li>`;
+  }
+} 
+
+const allCheckbox = document.querySelectorAll('.js-check');
+
+function handleCheck (event) {
+  const id = event.target.id;
+  console.log(id);
+  tasks[id].completed = !tasks[id].completed;
+  console.log(tasks);
+  renderTasks();
+}
+
+for(const check of allCheckbox) {
+  check.addEventListener('change', handleCheck);
+}
+
+// for (const task of tasks) {
+//   let checkbox = '<input= type="checkbox" class= "list">';
+//   if (task.completed) {
+//       checkbox = '<input= type="checkbox" checked>';
+//   }
+//   tasksList.innerHTML += `<li>${checkbox}${task.name}</li>`;
+//   const inputs = document.querySelectorAll ('.list');
+//   for (const input of inputs) {
+//     input.addEventListener ('click', handleClick);
+//     console.log ('click');
+
+//   }
+//   function handleClick (){
+//     if (tasks.completed === true) {
+//       tasks.completed === false;
+//       checkbox.classList.add ('tachado') 
+//     }
+//     else {
+//       tasks.completed === true;
+//       checkbox.classList.remove ('tachado') 
+      
+//     }
+//   }
+// }
+
 

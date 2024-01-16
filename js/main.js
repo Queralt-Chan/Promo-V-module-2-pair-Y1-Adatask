f9303ec78a0c4f466d5ba875b2cdc83a7b0e37c6
NaMillan <nazarethmillan@gmail.com>
2024-01-16 11:29:10 +0100

    listserver

--- js/main.js
+++ js/main.js
@@ -1,40 +1,93 @@
 'use strict';
+let tasks = [];
+/*const tasks = [
+  { name: 'Recoger setas en el campo', completed: true },
+  { name: 'Comprar pilas', completed: true },
+  { name: 'Poner una lavadora de blancos', completed: true },
+  {
+    name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
+    completed: false,
+  },
+];*/
 
-const tasksList  = document.querySelector('.task-list');
+const taskList = document.querySelector('.task-list');
+const btnFilter = document.querySelector('.js-btn-filter');
+const inputFilter = document.querySelector('.js-text-task-filter');
+const githubUser = '<tu_usuario_de_github_aqui>';
+const serverUrl = `https://dev.adalab.es/api/todo/`;
+const message=document.querySelector('.message');
 
-const tasks = [
-    { name: 'Recoger setas en el campo', completed: true },
-    { name: 'Comprar pilas', completed: true },
-    { name: 'Poner una lavadora de blancos', completed: true },
-    {
-      name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
-      completed: false,
-    },
-];
+function getDataApi (){
+fetch(serverUrl)
+.then((response) => response.json())
+.then ((data) => {
+  console.log(data);
+  tasks=data.results;
+  renderTasks(tasks);
+});
+}
+getDataApi ();
 
-
-for (const task of tasks) {
-  let checkbox = '<input= type="checkbox" class= "list">';
-  if (task.completed) {
-      checkbox = '<input= type="checkbox" checked>';
-  }
-  tasksList.innerHTML += `<li>${checkbox}${task.name}</li>`;
-  const inputs = document.querySelectorAll ('.list');
-  for (const input of inputs) {
-    input.addEventListener ('click', handleClick);
-    console.log ('click');
-
-  }
-  function handleClick (){
-    if (tasks.completed === true) {
-      tasks.completed === false;
-      checkbox.classList.add ('tachado') 
-    }
-    else {
-      tasks.completed === true;
-      checkbox.classList.remove ('tachado') 
-      
-    }
+const renderMessage () => {
+  const filterDataTrue = tasks.filter((results) => results.completed)
+  if (filterDataTrue){
+    message.innerHTML=`Tienes ${}`
   }
 }
 
+//renderTasks(tasks);
+//Completa el código;
+ //Guarda la respuesta obtenida enla variable para el listado de tareas: `tasks`
+
+function handleFilter(event) {
+  event.preventDefault();
+  const valueInput = inputFilter.value;
+  const arrayFilter = tasks.filter((task) => task.name.includes(valueInput));
+  console.log(arrayFilter);
+  renderTasks(arrayFilter);
+}
+btnFilter.addEventListener('click', handleFilter);
+
+const listenCheck = () => {
+  const allCheckbox = document.querySelectorAll('.js-check');
+  for (const check of allCheckbox) {
+    check.addEventListener('change', handleCheck);
+  }
+};
+const renderTasks = (tasks) => {
+  taskList.innerHTML = '';
+  for (let index = 0; index < tasks.length; index++) {
+    /*let classCss = tasks[index].completed ? 'tachado' : null;
+  taskList.innerHTML += `<li class= "${classCss}"> 
+    ${tasks[index].name}
+  </li>`;*/
+    if (tasks[index].completed) {
+      //tasks[index].completed === true
+      taskList.innerHTML += `<li class= "tachado"> 
+    <input type="checkbox" class= "js-check" id="${index}" checked>
+    ${tasks[index].name}
+  </li>`;
+    } else {
+      taskList.innerHTML += `<li> 
+    <input type="checkbox" class= "js-check" id="${index}" >
+    ${tasks[index].name}
+  </li>`;
+    }
+  }
+  listenCheck();
+};
+
+function handleCheck(event) {
+  const id = event.target.id;
+  console.log(id);
+  tasks[id].completed = !tasks[id].completed;
+  console.log(tasks);
+  renderTasks(tasks);
+}
+renderTasks(tasks);
+/*
+pintar elementos en html
+escuchar eventos 
+una vez escuchas cambias los datos basado en el evento
+volver a pintar, volver a escuchar
+*/
